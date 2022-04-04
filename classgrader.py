6fa9b0c4178e290b29c4b2949baeb220e2bbcfc7
ie-tech <102807199+ie-tech@users.noreply.github.com>
2022-04-04 17:54:32 +0300

    Add files via upload

---
+++ classgrader.py
@@ -0,0 +1,872 @@
+import os
+os.system('clear')
+import time as t
+print('''░i░e░-░t░e░c░h░''')
+t.sleep(1)
+print('''[̲̅W][̲̅E][̲̅L][̲̅C][̲̅O][̲̅M][̲̅E] [̲̅T][̲̅O] [̲̅M][̲̅Y] [̲̅T][̲̅O][̲̅O][̲̅L]''')
+tn = '\nMARKS CALCULATOR AND CLASS GRADER by ie-tech'
+ie = 'by ie-tech'
+#opinion = 'y'
+
+opinion = input('Would you like to continue(y/n): ')
+while opinion == 'n':
+    thanks = '🐑  🎀  𝓉𝒽𝒶𝓃𝓀𝓈 𝒻😍𝓇 𝓊𝓈𝒾𝓃𝑔 𝓂𝓎 𝓉🍩🏵𝓁  🎀  🐑'
+    print(thanks)
+       
+while opinion == 'y':
+   password = input('please enter the tool password\nPASSORD: '.upper()) 
+   t.sleep(1)
+   print('##checking....')
+   while password == 'ie-tech':
+    print('''𝘀𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆 𝘂𝗻𝗹𝗼𝗰𝗸𝗲𝗱..''')
+    os.system('clear')
+    options1 = int(input('\nchoose options(1/2/3/4),\n1.calculate total marks and average and grade per student,\n2.positionions.\n3.contact ie-tech\n4.exit,\n').upper())
+
+  
+     #STUDENTS POSITIONS FROM TOTAL MARKS
+    if options1 == 2:
+         print('\n1.students names list')
+         qtn = int(input('(1/2/3)> '))
+         if qtn == 1:
+             os.system('clear')
+             print('\nour lady of africa ss namilyango,\n s.3w 2022'.upper())
+             t.sleep(1)
+             #print('\nenter students total marks'.upper())
+             aberg = 'aber gladys tracy'.upper()
+             aberl = 'aber lynette'.upper()
+             abers = 'aber sharfshian'.upper()
+             abwin = 'abwin viola'.upper()
+             aguti = 'aguti dinnah'.upper()
+             ajambo = 'ajambo esther gracious'.upper()
+             anena = 'anena daisy'.upper()
+             angufiru = 'angufiru babra'.upper()
+             anguzu = 'anguzu micho richard'.upper()
+             ankunda = 'ankunda loyce'.upper()
+             aparo = 'aparo consolate olive'.upper()
+             apenjunga = 'apenjunga patience'.upper()
+             assimwe = 'asiimwe reid'.upper()
+             aweko = 'aweko justin'.upper()
+             ayebare = 'ayebare desire'.upper()
+             babirye = 'babirye ruth'.upper()
+             barak = 'barak valerian'.upper()
+             biira = 'biira peace'.upper()
+             bukirwa = 'bukirwa daniella'.upper()
+             byaidude = 'byaidude alvin'.upper()
+             byamukama1 = 'byamukama ben'.upper()
+             byamukama2 = 'byamukama henry'.upper()
+             desire = 'desire beckyline'.upper()
+             ebong = 'ebong timothy'.upper()
+             gum = 'gum nathan wonder'.upper()
+             higenyi = 'higenyi edgar'.upper()
+             ikion = 'ikion lillian'.upper()
+             irunva = 'irunva filma'.upper()
+             kansiime = 'kansiime doriicka'.upper()
+             katushabe = 'katushabe scovia'.upper()
+             kayanja = 'kayanja ryan'.upper()
+             kazooba = 'kazooba jean mary'.upper()
+             kirabo = 'kirabo mellinia'.upper()
+             kibirige = 'kibirige cedrick'.upper()
+             kisssa = 'kissa deborah'.upper()
+             kizza = 'kizza james okello'.upper()
+             kobu = 'kobusinge mercy abooki'.upper()
+             kyosaba = 'kyosaba marion nakibinga'.upper()
+             lamunu = 'lamunu sharon otim'.upper()
+             letasic = 'letasi clare valence'.upper()
+             letasih = 'letasi hope'.upper()
+             luvunia = 'luvunia david'.upper()
+             mitch = 'mitch joseph'.upper()
+             mugabi = 'mugabi dagalas'.upper()
+             mugisha = 'mugisha francis'.upper()
+             mukisa = 'mukisa jeremiah'.upper()
+             mutesi = 'mutesi sandra'.upper()
+             mwakaa = 'mwaka allan'.upper()
+             mwakae = 'mwaka emmanuel obol'.upper()
+             dauda = 'mwima dauda'.upper()
+             nabifa = 'nabifa joyce'.upper()
+             nabwire = 'nabwire erina'.upper()
+             naggita = 'naggita kirabo daisy'.upper()
+             nakaddu = 'nakaddu confort'.upper()
+             nakato = 'nakato lillian'.upper()
+             nakawoza = 'nakawoza gabriella stacy'.upper()
+             namutosi = 'namutosi given were'.upper()
+             nanfuka = 'nanfuka nsobra'.upper()
+             nansereko = 'nansereko belinda'.upper()
+             nansereko = 'nansereko belinda'.upper()
+             nasasira = 'nasasira dericky'.upper()
+             ndagire = 'ndagire samali'.upper()
+             nsereko = 'nsereko fahad'.upper()
+             nuwaha = 'nuwaha joel jeremiah'.upper()
+             nyakato = 'nyakato martha crystal'.upper()
+             ojullo = 'ojullo nicholas'.upper()
+             okolong = 'okolong adrian oneko'.upper()
+             okonya = 'okonya isaac'.upper()
+             okot = 'okot eric'.upper()
+             opolot = 'opolot emmanuel'.upper()
+             oringtho = 'oringtho anna nirwoth'.upper()
+             otim = 'otim albert'.upper()
+             powell = 'rwothomio king powell'.upper()
+             ssekatawa = 'ssekatawa richard'.upper()
+             newton = 'turyomurygyendo newton'.upper()
+             wamono = 'wamono bob'.upper()
+             wanok = 'wanok godwin'.upper()
+             #CLASS NAMES LIST
+             print(aberg)
+             print(aberl) 
+             print(abers) 
+             print(abwin)
+             print(ajambo)
+             print(anena)
+             print(anguzu) 
+             print(ankunda) 
+             print(aparo)
+             print(assimwe)
+             print(aweko)
+             print(ayebare)
+             print(barak)
+             print(biira)
+             print(bukirwa) 
+             print(byaidude)
+             print(byamukama1) 
+             print(byamukama2) 
+             print(desire) 
+             print(ebong)
+             print(gum)
+             print(higenyi) 
+             print(ikion)
+             print(kansiime) 
+             print(katushabe) 
+             print(kayanja)
+             print(kazooba) 
+             print(kibirige) 
+             print(kisssa) 
+             print(kizza) 
+             print(kobu)
+             print(kyosaba) 
+             print(lamunu) 
+             print(letasic) 
+             print(luvunia)
+             print(mitch) 
+             print(mugabi)
+             print(mugisha)
+             print(mukisa)
+             print(mwaka) 
+             print(dauda) 
+             print(nabifa)
+             print(naggita)
+             print(nakato) 
+             print(nakawoza)
+             print(namutosi)
+             print(nanfuka)
+             print(nansereko) 
+             print(nasasira) 
+             print(ndngire)
+             print(nsereko)
+             print(nuwaha)
+             print(nyakato) 
+             print(ojullo) 
+             print(okolong) 
+             print(okonya)
+             print(okot) 
+             print(oringtho) 
+             print(otim)
+             print(powell)
+             print(newton)
+             print(wamono)
+             print(wanok)
+
+             
+
+   
+
+        #OPTIONS1 ...2 (CONTACT IE-TECH)
+
+    if options1 == 3:
+        os.system('clear')
+        print('thanks for trying to reach ie-tech')
+        t.sleep(1)
+        print('\nEnter (y) to contact on whatsapp')
+        media = input('\n')
+        if media == 'y':
+            print('''𝔠𝔬𝔫𝔫𝔢𝔠𝔱𝔦𝔫𝔤 𝔦𝔢-𝔱𝔢𝔠𝔥....\n''')
+            t.sleep(1)
+            os.system("xdg-open https://wa.me/+256776515577")
+
+        else:
+            print('unrecognised command!!!!..')
+    if options1 == 4:
+        print('THANKS FOR USING MY TOOL......ba bye')
+        t.sleep(1)
+        print('have a nice day.....')
+        #TOTAL MARKS AND AVERAGE MARKS
+    if options1 == 1:
+        print('CALCULATE TOTAL MARKS AND AVERAGE PER STUDENT',ie)
+        print('please enter optional subjects with their first three letters')
+        print('ENTER YOUR MARKS')
+        mtc = 'mathematics  '
+        eng = 'english      '
+        bio = 'biology      '
+        chem = 'chemistry    '
+        phy = 'physics      '
+        hist = 'history      '
+        geo = 'geography    '
+        
+        opt1 = input('optional 1    :')
+        opt2 = input('optional 2    :')
+        opt3 = input('optional 3   :')
+        m1 =int(input(mtc.upper()+'  :'))
+        m2 =int(input(eng.upper()+'  :'))
+        m3 =int(input(bio.upper()+'  :'))
+        m4 =int(input(chem.upper()+'  :'))
+        m5 =int(input(phy.upper()+'  :'))
+        m6 =int(input(hist.upper()+'  :'))
+        m7 =int(input(geo.upper()+'  :'))
+        print('\nENTER OPTIONALS MARKS\n')
+        m8 =int(input(opt1.upper()+'  :'))
+        m9 =int(input(opt2.upper()+'  :'))
+        m10 =int(input(opt3.upper()+'  :'))
+        os.system('clear')
+        totalmarks = (m1+m3+m4+m5+m6+m7+m8+m9+m10)
+        averagemarks = (totalmarks)/10
+        os.system('clear')
+        print('\ntotal marks is ',totalmarks)
+        print('\nAverage marks is ',averagemarks)
+        
+        
+        t.sleep(0)
+        #if m1 or m2 or m3 or m4 or m5 or m6 or m7 or m8 or m9 or m10 > 100:
+            #print('an error occured when entering!!!!')
+           # t.sleep(1)
+           # print('\nmarks should not exceed 100!!!')
+
+        #DEF M1
+        print('\ncompulsory subjects'.upper())
+        if m1 >= 80:
+            print(mtc.upper(),m1,'D1')
+        elif  m1 >= 75:
+            print(mtc.upper(),m1,'D2')
+        elif m1 >= 65:
+            print(mtc.upper(),m1,'C3')
+        elif  m1 >= 60:
+            print(mtc.upper(),m1,'C4')
+        elif m1 >= 55:
+            print(mtc.upper(),m1,'C5')
+        elif m1 >= 50:
+            print(mtc.upper(),m1,'C6')
+        elif m1 >= 45:
+            print(mtc.upper(),m1,'P7')
+        elif m1 >= 35:
+            print(mtc.upper(),m1,'P8')
+        elif m1 <= 34:
+            print(mtc.upper(),m1,'F9')
+        else:
+            print('an error occured when entering!!!!')
+
+        #DEF M2
+        if m2 >= 80:
+            print(eng.upper(),m2,'D1')
+        elif  m2 >= 75:
+            print(eng.upper(),m2,'D2')
+        elif m2 >= 65:
+            print(eng.upper(),m2,'C3')
+        elif m2 >= 60:
+            print(eng.upper(),m2,'C4')
+        elif m2 >= 55:
+            print(eng.upper(),m2,'C5')
+        elif m2 >= 50:
+            print(eng.upper(),m2,'C6')
+        elif m2 >= 45:
+            print(eng.upper(),m2,'P7')
+        elif m2 >= 35:
+            print(eng.upper(),m2,'P8')
+        elif m2 <= 34:
+            print(eng.upper(),m2,'F9')
+        else:
+            print('an error occured when entering!!!!')
+
+        #DEF M3
+        if m3 >= 80:
+            print(bio.upper(),m3,'D1')
+        elif  m3 >= 75:
+            print(bio.upper(),m3,'D2')
+        elif m3 >= 65:
+            print(bio.upper(),m3,'C3')
+        elif  m3 >= 60:
+            print(bio.upper(),m3,'C4')
+        elif m3 >= 55:
+            print(bio.upper(),m3,'C5')
+        elif m3 >= 50:
+            print(bio.upper(),m3,'C6')
+        elif m3 >= 45:
+            print(bio.upper(),m3,'P7')
+        elif m3 >= 35:
+            print(bio.upper(),m3,'P8')
+        elif m3 <= 34:
+            print(bio.upper(),m3,'F9')
+        else:
+            print('an error occured when entering!!!!')
+
+        #DEF M4
+        if m4 >= 80:
+            print(chem.upper(),m4,'D1')
+        elif  m4 >= 75:
+            print(chem.upper(),m4,'D2')
+        elif m4 >= 65:
+            print(chem.upper(),m4,'C3')
+        elif  m4 >= 60:
+            print(chem.upper(),m4,'C4')
+        elif m4 >= 55:
+            print(chem.upper(),m4,'C5')
+        elif m4 >= 50:
+            print(chem.upper(),m4,'C6')
+        elif m4 >= 45:
+            print(chem.upper(),m4,'P7')
+        elif m4 >= 35:
+            print(chem.upper(),m4,'P8')
+        elif m4 <= 34:
+            print(chem.upper(),m4,'F9')
+        else:
+            print('an error occured when entering!!!!')
+
+        #DEF M5
+        if m5 >= 80:
+            print(phy.upper(),m5,'D1')
+        elif  m5 >= 75:
+            print(phy.upper(),m5,'D2')
+        elif m5 >= 65:
+            print(phy.upper(),m5,'C3')
+        elif  m5 >= 60:
+            print(phy.upper(),m5,'C4')
+        elif m5 >= 55:
+            print(phy.upper(),m5,'C5')
+        elif m5 >= 50:
+            print(phy.upper(),m5,'C6')
+        elif m5 >= 45:
+            print(phy.upper(),m5,'C7')
+        elif m5 >= 35:
+            print(phy.upper(),m5,'P8')
+        elif m5 <= 34:
+            print(phy.upper(),m5,'F9')
+        else:
+            print('an error occured when entering!!!!')
+
+        #DEF M6
+        if m6 >= 80:
+            print(hist.upper(),m6,'D1')
+        elif  m6 >= 75:
+            print(hist.upper(),m6,'D2')
+        elif m6 >= 65:
+            print(hist.upper(),m6,'C3')
+        elif  m6 >= 60:
+            print(hist.upper(),m6,'C4')
+        elif m6 >= 55:
+            print(hist.upper(),m6,'C5')
+        elif m6 >= 50:
+            print(hist.upper(),m6,'C6')
+        elif m6 >= 45:
+            print(hist.upper(),m6,'P7')
+        elif m6 >= 35:
+            print(hist.upper(),m6,'P8')
+        elif m6 <= 34:
+            print(hist.upper(),m6,'F9')
+        else:
+            print('an error occured when entering!!!!')
+
+        #DEF M7
+        if m7 >= 80:
+            print(geo.upper(),m7,'D1')
+        elif  m7 >= 75:
+            print(geo.upper(),m7,'D2')
+        elif m7 >= 65:
+            print(geo.upper(),m7,'C3')
+        elif  m7 >= 60:
+            print(geo.upper(),m7,'C4')
+        elif m7 >= 55:
+            print(geo.upper(),m7,'C5')
+        elif m7 >= 50:
+            print(geo.upper(),m7,'C6')
+        elif m7 >= 45:
+            print(geo.upper(),m7,'P7')
+        elif m7 >= 35:
+            print(geo.upper(),m7,'P8')
+        elif m7 <= 34:
+            print(geo.upper(),m7,'F9')
+        else:
+            print('an error occured when entering!!!!')
+
+         #DEF M8
+        print('\noptionals'.upper())    
+        if m8 >= 80:
+            print(opt1.upper(),m8,'D1')
+        elif  m8 >= 75:
+            print(opt1.upper(),m8,'D2')
+        elif m8 >= 65:
+            print(opt1.upper(),m8,'C3')
+        elif  m8 >= 60:
+            print(opt1.upper(),m8,'C4')
+        elif m8 >= 55:
+            print(opt1.upper(),m8,'C5')
+        elif m8 >= 50:
+            print(opt1.upper(),m8,'C6')
+        elif m8 >= 45:
+            print(opt1.upper(),m8,'P7')
+        elif m8 >= 35:
+            print(opt1.upper(),m8,'P8')
+        elif m8 <= 34:
+            print(opt1.upper(),m8,'F9')
+        else:
+            print('an error occured when entering!!!!')
+
+        #DEF M9
+        if m9 >= 80:
+            print(opt2.upper(),m9,'D1')
+        elif  m9 >= 75:
+            print(opt2.upper(),m9,'D2')
+        elif m9 >= 65:
+            print(opt2.upper(),m9,'C3')
+        elif  m9 >= 60:
+            print(opt2.upper(),m9,'C4')
+        elif m9 >= 55:
+            print(opt2.upper(),m9,'C5')
+        elif m9 >= 50:
+            print(opt2.upper(),m9,'C6')
+        elif m9 >= 45:
+            print(opt2.upper(),m9,'P7')
+        elif m9 >= 35:
+            print(opt2.upper(),m9,'P8')
+        elif m9 <= 34:
+            print(opt2.upper(),m9,'F9')
+        else:
+            print('an error occured when entering!!!!')
+
+        #DEF M10
+        if m10 >= 80:
+            print(opt3.upper(),m10,'D1')
+        elif  m10 >= 75:
+            print(opt3.upper(),m10,'D2')
+        elif m10 >= 65:
+            print(opt3.upper(),m10,'C3')
+        elif  m10 >= 60:
+            print(opt3.upper(),m10,'C4')
+        elif m10 >= 55:
+            print(opt3.upper(),m10,'C5')
+        elif m10 >= 50:
+            print(opt3.upper(),m10,'C6')
+        elif m10 >= 45:
+            print(opt3.upper(),m10,'P7')
+        elif m10 >= 35:
+            print(opt3.upper(),m10,'P8')
+        elif m10 <= 34:
+            print(opt3.upper(),m10,'F9')
+        else:
+            print('an error occured when entering!!!!')
+    else:
+        print('')
+        #TABLE
+        print()
+input('Would you like to continue(y/n): ')
+    
+
+
+
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#print('''𝓣𝓗𝓐𝓝𝓚𝓢 𝓕𝓞𝓡 𝓤𝓢𝓘𝓝𝓖 𝓜𝓨 𝓣𝓞𝓞𝓛''',ie)
